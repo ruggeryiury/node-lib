e30538325b738bd733861c7cb485b965551aea18
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2025-08-06 15:44:59 -0300

    v1.2.1: Optimized hash generator method

--- src/lib/hash/createHash.ts
+++ src/lib/hash/createHash.ts
@@ -10,12 +10,11 @@ export type AllHashAlgorithms = 'md4' | 'md5' | 'mdc2' | 'ripemd160' | 'rmd160' 
  * - - - -
  * @param {Buffer} input The input to hash.
  * @param {AllHashAlgorithms} [algorithm] The hash algorithm to use. Default is `'sha256'`.
- * @param {BinaryToTextEncoding} [digest] The output encoding for the hash. Default is `'hex'`.
+ * @param {BinaryToTextEncoding} [outputEncoding] The output encoding for the hash. Default is `'hex'`.
  * @returns {string} The resulting hash string.
  */
-export const createHashFromBuffer = (input: Buffer, algorithm: AllHashAlgorithms = 'sha256', digest: BinaryToTextEncoding = 'hex'): string => {
-  const hash = nodeCreateHash(algorithm).update(input).digest(digest)
-  return hash
+export const createHashFromBuffer = (input: Buffer, algorithm: AllHashAlgorithms = 'sha256', outputEncoding: BinaryToTextEncoding = 'hex'): string => {
+  return nodeCreateHash(algorithm).update(input).digest(outputEncoding)
 }
 
 /**
@@ -26,16 +25,16 @@ export const createHashFromBuffer = (input: Buffer, algorithm: AllHashAlgorithms
  * - - - -
  * @param {FilePathLikeTypes} filePath The path to the file.
  * @param {AllHashAlgorithms} [algorithm] The hash algorithm to use. Default is `'sha256'`.
- * @param {BinaryToTextEncoding} [outEncoding] The output encoding for the hash. Default is `'hex'`.
+ * @param {BinaryToTextEncoding} [outputEncoding] The output encoding for the hash. Default is `'hex'`.
  * @returns {Promise<string>} A promise that resolves to the resulting hash string.
  */
-export const createHashFromFile = async (filePath: FilePathLikeTypes, algorithm: AllHashAlgorithms = 'sha256', outEncoding: BinaryToTextEncoding = 'hex'): Promise<string> =>
+export const createHashFromFile = async (filePath: FilePathLikeTypes, algorithm: AllHashAlgorithms = 'sha256', outputEncoding: BinaryToTextEncoding = 'hex'): Promise<string> =>
   new Promise<string>((resolve, reject) => {
     const path = pathLikeToFilePath(filePath)
     const hash = nodeCreateHash(algorithm)
     const stream = createReadStream(path.path)
 
     stream.on('data', (chunk) => hash.update(chunk))
-    stream.on('end', () => resolve(hash.digest(outEncoding)))
+    stream.on('end', () => resolve(hash.digest(outputEncoding)))
     stream.on('err', reject)
   })
